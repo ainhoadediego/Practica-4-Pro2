d9f7de3e004d90fae58a346652447faf95d9f3a7
luuVega <160024089+luuVega@users.noreply.github.com>
2024-05-01 17:34:34 +0200

    Update course.py

--- course.py
+++ course.py
@@ -2,156 +2,130 @@
 # Ainhoa de Diego Silva. ainhoa.dediego.silva@udc.es
 
 class Course:
-
     """Class that initializes the Course object. 
     
     Objects of this class represent courses of different languages and levels
-stored with their data regarding type, duration, price, and number of enrolled students.
+    stored with their data regarding type, duration, price, and number of enrolled students.
 
-Attributes
-----------
-name: str
-    Identifies a particular course object.
-    Name used solely to identify the object, but there may be multiple courses with the same name.
+    Attributes
+    ----------
+    name: str
+        Identifies a particular course object.
+        Name used solely to identify the object, but there may be multiple courses with the same name.
 
-duration: int
-    Number of hours to complete the course.
+    duration: int
+        Number of hours to complete the course.
 
-number_students: int
-    Number of students enrolled in the course.
+    number_students: int
+        Number of students enrolled in the course.
 
-level: str
-    Language level to quantify the course material (A, B, C...).
+    level: str
+        Language level to quantify the course material (A, B, C...).
 
-language: str
-    Language on which the class will be taught.
+    language: str
+        Language in which the class will be taught.
 
-price: float
-    Price to be paid per student.
+    price: float
+        Price to be paid per hour and student.
 
-Methods
--------
-@property
-name:
-    Method that returns the course name.
+    benefit: float
+        Total income obtained by the course (price * duration * number_students).
 
-@name.setter
-name(self, value: str):
-    Method that updates the course name, giving it a new one.
+    Methods
+    -------
+    @property
+    name:
+        Method that returns the course name.
 
-@property
-duration:
-    Method that returns the course duration.
+    @property
+    duration:
+        Method that returns the course duration.
 
-@property
-number_students:
-    Method that returns the number of students enrolled in the course.
+    @property
+    number_students:
+        Method that returns the number of students enrolled in the course.
 
-@number_students.setter
-number_students(self, value:int):
-    Method that updates the number of enrolled students, giving it a new value.
+    @property
+    level:
+        Method that returns the course level.
 
-@property
-level:
-    Method that returns the course level.
+    @property
+    language:
+        Method that returns the language to be taught in the course.
 
-@property
-language:
-    Method that returns the language to be taught in the course.
+    @property
+    price:
+        Method that returns the price to be paid per enrolled student.
 
-@property
-price:
-    Method that returns the price to be paid per enrolled student.
+    @property
+    benefit:
+        Method that returns the benefit obtained.
 
-__eq__(self, other_course: 'Course'):
-    Method that returns a boolean value when comparing parameters (name, level, and language) to determine if they are equal.
+    __eq__(self, other_course: 'Course'):
+        Method that returns a boolean value when comparing parameters (name, level, and language) to determine if they are equal.
 
-g_benefit(self, other_course: 'Course'):
-    Method that returns a boolean value when comparing if the current course provides greater benefit than other_course.
+    ge_benefit(self, other_course: 'Course'):
+        Method that returns a boolean value when comparing if the current course provides greater or equeal benefit than other_course.
 
-eq_benefit(self, other_course: 'Course'):
-    Method that returns a boolean value when comparing if the current course provides equal benefit to other_course.
+    label(self):
+        Method that returns a string with the characteristics of the name, level, and language of the course.
 
-label(self):
-    Method that returns a string with the characteristics of the name, level, and language of the course.
-
-__str__(self):
-    Method that returns all the characteristics of the Course object in question.
-
+    __str__(self):
+        Method that returns all the characteristics of the Course object in question.
     """ 
-     
+
     def __init__(self, name: str, duration: int, number_students: int, level: str, language: str, price: float):
-        '''Assigns the different attributes to the Course object.
+        """Assigns the different attributes to the Course object.
  
-            Parameters
-            ----------
-            name: str
-                Identifies a particular course object.
-                Name used solely to identify the object, but there may be multiple courses with the same name.
+        Parameters
+        ----------
+        name: str
+            Identifies a particular course object.
+            Name used solely to identify the object, but there may be multiple courses with the same name.
 
-            duration: int
-                Number of hours to complete the course.
+        duration: int
+            Number of hours to complete the course.
 
-            number_students: int
-                Number of students enrolled in the course.
+        number_students: int
+            Number of students enrolled in the course.
 
-            level: str
-                Language level to quantify the course material (A, B, C...).
+        level: str
+            Language level to quantify the course material (A, B, C...).
 
-            language: str
-                Language on which the class will be taught.
+        language: str
+            Language on which the class will be taught.
 
-            price: float
-                Price to be paid per student.
+        price: float
+            Price to be paid per student.
 
-            Returns
-            -------
-            None.
-            '''
-
-
+        Returns
+        -------
+        None.
+        """       
         self._name = name
         self._duration = duration
         self._number_students = number_students
         self._level = level
         self._language = language
         self._price = price
+        self._benefit = self._price * self._duration * self._number_students
 
     @property
     def name(self):
-       """Returns what is stored in the variable self._name.
+        """Returns what is stored in the variable self._name.
 
         Returns
         --------
         name: str
-           Identifying name of the course.
+            Identifying name of the course.
 
         Given by default.
         """
-
-       return self._name
-    
-    @name.setter
-    def name(self, value):
-        """Modifies the value of what is stored in the variable self._name.
-
-            Parameters
-            ----------
-            value: str
-                New name to be set for the course.
-
-            Returns
-            --------
-            None
-
-            It changes when two courses have the same name, the company name is added.
-            """
-
-        self._name = str(value)
+        return self._name
     
     @property
     def duration(self):
-       """Returns what is stored in the variable self._duration.
+        """Returns what is stored in the variable self._duration.
 
         Returns
         --------
@@ -160,7 +134,7 @@ __str__(self):
 
         Given by default.
         """
-       return self._duration
+        return self._duration
     
     @property
     def number_students(self):
@@ -173,30 +147,7 @@ __str__(self):
 
         Given by default.
         """
-
         return self._number_students
-    
-    @number_students.setter
-    def number_students(self, value):
-       """Modifies the value of what is stored in the variable self._number_students.
-
-            Parameters
-            ----------
-            number_students: int
-                Value of the number of students enrolled in the course.
-
-            Returns
-            --------
-            None
-
-            It changes when two courses are equal, so the students from both groups are added
-            in the course with the highest profits.
-        """
-
-       if value >= 0:
-            self._number_students = int(value)
-       else:
-            print("The number of students must be greater than 0.")
     
     @property
     def level(self):
@@ -209,7 +160,6 @@ __str__(self):
 
         Given by default.
         """
-
         return self._level
 
     @property
@@ -223,7 +173,6 @@ __str__(self):
 
         Given by default.
         """
-
         return self._language
     
     @property
@@ -232,13 +181,26 @@ __str__(self):
 
         Returns
         --------
-        price: int
+        price: float
            Monetary value that each student should contribute according to the course.
 
         Given by default.
         """
-
         return self._price
+    
+    @property
+    def benefit(self):
+        """Returns what is stored in the variable self._benefit.
+
+        Returns
+        --------
+        price: float
+           Monetary value obtained by the course.
+           It is calculated by price * duration * number_students
+
+        Given by default.
+        """
+        return self._benefit
     
     def __eq__(self, other_course: 'Course'):
         """Method that returns a boolean value when comparing the current course with another.
@@ -249,58 +211,35 @@ __str__(self):
 
         Returns True if the parameters (name, level, and language) are the same in both courses.
         """
-
         return self._name == other_course.name and self._level == other_course.level and self._language == other_course.language
     
-    def g_benefit(self, other_course: 'Course'):
+    def ge_benefit(self, other_course: 'Course'):
         """Method that returns a boolean value when comparing the benefits of the current course with another.
 
         Returns
         --------
         bool
 
-       Returns True if the self course provides more benefits than the other_course.
-       A course is more profitable than another if it generates more money:
-       we calculate the price per hour and multiply it by the number of students.
-
-       """
-
-        return (self._price/self._duration * self._number_students) > (other_course.price/other_course.duration * other_course.number_students)
-    
-    def eq_benefit(self, other_course: 'Course'):
-        """Method that returns a boolean value when comparing the current course with another.
-
-        Returns
-        --------
-        bool
-
-        Returns True if the self course provides the same benefits as the other_course.
-        A course is as profitable as another if it generates the same amount of money:
-        we calculate the price per hour and multiply it by the number of students.
+        Returns True if the self course provides more or the same benefits than the other_course.
         """
-        return (self._price/self._duration * self._number_students) == (other_course.price/other_course.duration * other_course.number_students)
+        return self._benefit >= other_course.benefit
     
     def label(self):
         """Returns a string with information collected about the current course.
 
-            Returns
-            --------
-            str:
-                Returns the name, level, and language of the course in question.
-            """
-
+        Returns
+        --------
+        str
+            Returns the name, level, and language of the course in question.
+        """
         return f'{self._name}_{self._level}_{self._language}'
     
     def __str__(self):
-         """Returns a string with all the information gathered about the current course.
+        """Returns a string with all the information gathered about the current course.
 
-            Returns
-            --------
-            str:
-                Returns all the information set about the course in question.
-            """
-
-         return f"{self._name}, {self._duration}, {self._number_students}, {self._level}, {self._language}, {self._price}"
-    
-    
-
+        Returns
+        --------
+        str:
+            Returns all the information set about the course in question.
+        """        
+        return f"{self._name}, {self._duration}, {self._number_students}, {self._level}, {self._language}, {self._price}"
